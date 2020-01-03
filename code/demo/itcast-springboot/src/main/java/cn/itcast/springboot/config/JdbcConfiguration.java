6e9e5fdf80d2007711b6923c19fa3fdb9b72593b
Guo-Xinyan <2208708593@qq.com>
2020-01-03 19:40:53 +0800

    properties

--- code/demo/itcast-springboot/src/main/java/cn/itcast/springboot/config/JdbcConfiguration.java
+++ code/demo/itcast-springboot/src/main/java/cn/itcast/springboot/config/JdbcConfiguration.java
@@ -1,7 +1,10 @@
 package cn.itcast.springboot.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -10,25 +13,41 @@ import javax.sql.DataSource;
 
 
 @Configuration //声明一个类是java配置类
-@PropertySource("classpath:jdbc.properties")
+@EnableConfigurationProperties(JdbcProperties.class)
+//@PropertySource("classpath:jdbc.properties")
 public class JdbcConfiguration {
 
-    @Value("${jdbc.url}")
+    /*@Value("${jdbc.url}")
     String url;
     @Value("${jdbc.driverClassName}")
     String driverClassName;
     @Value("${jdbc.username}")
     String username;
     @Value("${jdbc.password}")
-    String password;
+    String password;*/
+    //@Autowired 第一种
+    private JdbcProperties jdbcProperties;
+//第二种 构造方法
+    public JdbcConfiguration(JdbcProperties jdbcProperties){
+        this.jdbcProperties = jdbcProperties;
+    }
 
+   /* @Bean //把方法的返回值注入到bean
+    public DataSource dataSource() { //第三种，参数传入
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setUrl(jdbcProperties.getUrl());
+        dataSource.setDriverClassName(jdbcProperties.getDriverClassName());
+        dataSource.setUsername(jdbcProperties.getUsername());
+        dataSource.setPassword(jdbcProperties.getPassword());
+        return dataSource;
+    }*/
+
+   //第四种
     @Bean
+    // 声明要注入的属性前缀，SpringBoot会自动把相关属性通过set方法注入到DataSource中
+    @ConfigurationProperties(prefix = "jdbc")
     public DataSource dataSource() {
         DruidDataSource dataSource = new DruidDataSource();
-        dataSource.setUrl(url);
-        dataSource.setDriverClassName(driverClassName);
-        dataSource.setUsername(username);
-        dataSource.setPassword(password);
         return dataSource;
     }
 }
