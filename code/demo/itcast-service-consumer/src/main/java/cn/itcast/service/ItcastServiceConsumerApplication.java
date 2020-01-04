3062517af50cc5b497d112e3e3f32b411b3f9af3
Guo-Xinyan <2208708593@qq.com>
2020-01-04 15:33:12 +0800

    ribbon

--- code/demo/itcast-service-consumer/src/main/java/cn/itcast/service/ItcastServiceConsumerApplication.java
+++ code/demo/itcast-service-consumer/src/main/java/cn/itcast/service/ItcastServiceConsumerApplication.java
@@ -2,15 +2,19 @@ package cn.itcast.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableCircuitBreaker //开启熔断器
 public class ItcastServiceConsumerApplication {
 
 	@Bean
+	@LoadBalanced  //开启负载均衡
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
--- code/demo/itcast-service-consumer/src/main/java/cn/itcast/service/controller/UserController.java
+++ code/demo/itcast-service-consumer/src/main/java/cn/itcast/service/controller/UserController.java
@@ -2,6 +2,8 @@ package cn.itcast.service.controller;
 
 import cn.itcast.service.pojo.User;
 
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -16,23 +18,36 @@ import java.util.List;
 
 @Controller
 @RequestMapping("consumer/user")
+@DefaultProperties(defaultFallback = "fallBackMethod") // 指定一个类的全局熔断方法
 public class UserController {
 
     @Autowired
     private RestTemplate restTemplate;
-    @Autowired
-    private DiscoveryClient discoveryClient; // eureka客户端，可以获取到eureka中服务的信息
+   // @Autowired
+    //private DiscoveryClient discoveryClient; // eureka客户端，可以获取到eureka中服务的信息
     @GetMapping
     @ResponseBody
-    public User queryUserById(@RequestParam("id") Long id){
+    @HystrixCommand //(fallbackMethod = "queryUserByIdFallBack")
+    public String queryUserById(@RequestParam("id") Long id){
         // 根据服务名称，获取服务实例。有可能是集群，所以是service实例集合
-        List<ServiceInstance> instances = discoveryClient.getInstances("service-provider");
+        //List<ServiceInstance> instances = discoveryClient.getInstances("service-provider");
         // 因为只有一个Service-provider。所以获取第一个实例
-        ServiceInstance instance = instances.get(0);
+        //ServiceInstance instance = instances.get(0);
         // 获取ip和端口信息，拼接成服务地址
-        String baseUrl = "http://" + instance.getHost() + ":" + instance.getPort() + "/user/" + id;
-        User user = this.restTemplate.getForObject(baseUrl, User.class);
+        //String baseUrl = "http://" + instance.getHost() + ":" + instance.getPort() + "/user/" + id;
+        String baseUrl = "http://service-provider/user/" + id;
+        String user = this.restTemplate.getForObject(baseUrl, String.class);
         return user;
+    }
+
+    /**
+     * 熔断方法
+     * 返回值要和被熔断的方法的返回值一致
+     * 熔断方法不需要参数
+     * @return
+     */
+    public String fallBackMethod(){
+        return "请求繁忙，请您稍后再试！";
     }
 
 }
