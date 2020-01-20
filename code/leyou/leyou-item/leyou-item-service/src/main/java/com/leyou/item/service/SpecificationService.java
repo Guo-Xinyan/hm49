a5644a13572e327e66040534b27b0ad7b16cc3ba
Guo-Xinyan <2208708593@qq.com>
2020-01-21 00:02:58 +0800

    Goods

---
+++ code/leyou/leyou-item/leyou-item-service/src/main/java/com/leyou/item/service/SpecificationService.java
@@ -0,0 +1,29 @@
+package com.leyou.item.service;
+
+import com.leyou.item.mapper.SpecGroupMapper;
+import com.leyou.item.mapper.SpecParamMapper;
+import com.leyou.item.pojo.SpecGroup;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SpecificationService {
+    @Autowired
+    private SpecGroupMapper groupMapper;
+
+    @Autowired
+    private SpecParamMapper paramMapper;
+
+    /**
+     * 根据分类id查询分组
+     * @param cid
+     * @return
+     */
+    public List<SpecGroup> queryGroupsByCid(Long cid) {
+        SpecGroup specGroup = new SpecGroup();
+        specGroup.setCid(cid);
+        return this.groupMapper.select(specGroup);
+    }
+}
